0414ef932133c35aabdc99efff1fb54c17dc37c1
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2022-02-23 18:41:31 -0500

    Initial commit
    created React app and added a header with logo and title

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+        <img src={logo} className="img-fluid" alt="logo" />
+        <h1>Dictionary</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
