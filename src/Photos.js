1cbb0a1bb01e42457987f57ace089d9265bb70e5
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2022-03-03 20:52:52 -0500

    changed styling of Photos to include a shadow instead of border

--- src/Photos.js
+++ src/Photos.js
@@ -3,13 +3,13 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-    if (props.photos) {
+    if (props.photos.length > 0) {
         return (
             <div className="Photos">
                 <div className="row">
                     {props.photos.map(function(photo, index) {
                         return (
-                            <div className="col-4" key={index}>
+                            <div className="col-4 photo-grid" key={index}>
                                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                                     <img src={photo.src.landscape} className="img-fluid" alt={photo.alt} />
                                 </a>
