f06cc73860befdd92ae13f9a26be7172f05939d1
chevonnesmellie <95383005+chevonnesmellie@users.noreply.github.com>
2022-03-03 20:54:48 -0500

    corrected issues for deployment

--- src/Photos.js
+++ src/Photos.js
@@ -3,13 +3,13 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-    if (props.photos.length > 0) {
+    if (props.photos) {
         return (
             <div className="Photos">
                 <div className="row">
                     {props.photos.map(function(photo, index) {
                         return (
-                            <div className="col-4 photo-grid" key={index}>
+                            <div className="col-4" key={index}>
                                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                                     <img src={photo.src.landscape} className="img-fluid" alt={photo.alt} />
                                 </a>
